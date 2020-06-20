39bbb72f7caa4eeb9cf0b7cc334ae14fce0c52b4
kodola <a.kodzis13@gmail.com>
2020-06-20 22:21:11 +0200

    state initialization, fetch data, form creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Nav from "./Nav";
 import Search from "./Search";
 
 const App = () => {
+    const [movies, setMovie] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const key = '1f6701f4695b66698a043fb831db39e9';
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${searchTerm}`)
+            .then(data => data.json())
+            .then(data => {
+                console.log(data);
+                setMovie([...data.results])
+            })
+    };
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
   return (
     <div className="App">
       <Nav/>
-      <Search/>
+      <Search handleSubmit={handleSubmit} handleChange={handleChange}/>
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,10 +5,11 @@ const Search = (props) => {
         <div className="container">
             <div className="row my-5 justify-content-start">
                 <section className="col-md-4 offset-md-4">
-                    <form action="" >
+                    <form action="" onSubmit={props.handleSubmit}>
                         <div className="input">
                             <input
-                            placeholder="Search movie..." type="text"/>
+                            placeholder="Search movie..." type="text" onChange={props.handleChange}
+                            />
                         </div>
                     </form>
                 </section>
