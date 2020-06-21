f0298ca7d7d685de88a0d9315769ffa85e618413
kodola <a.kodzis13@gmail.com>
2020-06-21 23:37:10 +0200

    pagination

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React, {useState, useEffect} from 'react';
 import Nav from "./Nav";
 import Search from "./Search";
 import Results from "./Results";
-
+import Pagination from "./Pagination";
 
 const App = () => {
     const [movies, setMovie] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [totalRes, setTotalRes] = useState(0);
+    const [currPage, setCurrPage] = useState(1);
 
     const key = '1f6701f4695b66698a043fb831db39e9';
 
@@ -17,18 +19,32 @@ const App = () => {
             .then(data => {
                 console.log(data);
                 setMovie([...data.results]);
+                setTotalRes(data.total_results);
             })
     };
     const handleChange = (e) => {
         setSearchTerm(e.target.value);
     };
 
+    //pagination
+    const nextPage = (pageNo) => {
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${searchTerm}&page=${pageNo}`)
+            .then(data => data.json())
+            .then(data => {
+                console.log(data);
+                setMovie([...data.results]);
+                setCurrPage(pageNo);
+            })
+    };
+    const pages = Math.floor(totalRes / 20);
+
 
   return (
     <div className="App">
       <Nav/>
       <Search handleSubmit={handleSubmit} handleChange={handleChange}/>
-      <Results  movies={movies}/>
+      <Results movies={movies}/>
+        { totalRes > 20 ? <Pagination pages={pages} nextPage={nextPage} currPage={currPage}/> : '' }
     </div>
   );
 };
---
+++ src/components/Pagination.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Pagination = (props) => {
+    const pageLinks = [];
+
+    for (let i = 1; i <= props.pages + 1; i++){
+        let active = props.currPage == i ? 'active' : '';
+        pageLinks.push(<li className={`page-item ${active}`} key={i} onClick={() => props.nextPage(i)}><a href='#' className='page-link'>{i}</a></li>)
+    }
+    return (
+        <div className="container">
+            <div className="row">
+                <nav aria-label="Page navigation example">
+                    <ul className='pagination'>
+                        {props.currPage > 1 ? <li className={`page-item`} onClick={() => props.nextPage(props.currPage - 1)}><a href='#' className='page-link'>Prev</a></li> : ''}
+                        {/*{pageLinks}*/}
+                        {props.currPage < props.pages + 1 ? <li className={`page-item`} onClick={() => props.nextPage(props.currPage + 1)}><a href='#' className='page-link'>Next</a></li> : ''}
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
+};
+
+export default Pagination;
