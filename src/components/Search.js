b90d1ebced5d37c9a77a1a13606fa50065ef9a4c
kodola <a.kodzis13@gmail.com>
2020-06-22 21:37:19 +0200

    dropdown modify

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,17 @@
-import React, {useState} from "react";
+import React from "react";
 import Suggestions from "./Suggestions";
 
 const Search = (props) => {
-    const [suggestion, setSuggestion] = useState(true);
-    const hideSuggestions = () => {
-        setSuggestion(!suggestion);
-    };
     return (
         <div className="container">
             <div className="row my-5 justify-content-start">
                 <section className="col-md-4 offset-md-4">
                     <form action="" onSubmit={props.handleSubmit}>
-                        <div className="input">
-                            <input
-                            placeholder="Search movie..." type="text" onChange={props.handleChange}
-                            onClick={hideSuggestions}
-                            />
+                        <div className="input-group">
+                            <input className="form-control" aria-label="Text input with dropdown button"
+                            placeholder="Search movie..." type="text" onChange={props.handleChange}/>
                         </div>
-                        <div style={{display: suggestion ? 'block' : 'none'}}>
                         <Suggestions movies={props.movies} searchTerm={props.searchTerm} viewMovieInfo={props.viewMovieInfo}/>
-                        </div>
                     </form>
                 </section>
             </div>
--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Suggestions = (props) => {
 
+    const [suggestion, setSuggestion] = useState(false);
+    const showSuggestions = () => {
+        setSuggestion(!suggestion);
+    };
+
     const options = props.movies.map((movie, i) => (
         i < 5 &&
-        <button className="list-group-item list-group-item-action" key={i} onClick={() => props.viewMovieInfo(movie.id)}>{movie.title}</button>
+        <button className="dropdown-item" key={i} onClick={() => props.viewMovieInfo(movie.id)}>{movie.title}</button>
     ));
-    if (props.searchTerm !== null) {
         return (
-            <div className="list-group">
-                {options}
+            <div className="input-group-append">
+                <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" onClick={showSuggestions}>Dropdown
+                </button>
+                <div className="dropdown-menu" style={{display: suggestion ? 'block' : 'none'}}>
+                    {options}
+                </div>
             </div>
         )
-    }
-    if (props.searchTerm === false ) {
-        return null;
-    }
-
-
 };
 
 export default Suggestions;
